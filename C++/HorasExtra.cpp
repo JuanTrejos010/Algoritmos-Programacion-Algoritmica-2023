886bc9dc1ca48ea02425321d649a6c216daf65a3
Juan Trejos <79339661+JuanTrejos010@users.noreply.github.com>
2023-05-15 16:59:31 +0200

    Create HorasExtra.cpp

---
+++ C++/HorasExtra.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    float SalarioHoras, HorasExtrasD, HorasExtrasN, HorasExtrasDomD;
+    std::cout <<"Escribe el salario por horas.\n";
+    std::cin >>SalarioHoras;
+        std::cout <<"\nEscribe el salario por horas.\n";
+    std::cin >>SalarioHoras;
+}
