d8683a52d94a630a23a458a2c180693163b329ea
Juan Trejos <79339661+JuanTrejos010@users.noreply.github.com>
2023-05-28 23:25:25 -0500

    Update HorasExtra.cpp
    Hice que el calculador de horas extras funcionara

--- C++/HorasExtra.cpp
+++ C++/HorasExtra.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 
 int main() {
-    float SalarioHoras, HorasExtrasD, HorasExtrasN, HorasExtrasDomD;
-    std::cout <<"Escribe el salario por horas.\n";
+    float SalarioHoras, HorasExtrasD, HorasExtrasN, HorasExtrasDomD, SalarioTotal;
+    std::cout <<"Programa para calcular el salario por horas extras.\nEscribe el salario por horas.\n";
     std::cin >>SalarioHoras;
-        std::cout <<"\nEscribe el salario por horas.\n";
-    std::cin >>SalarioHoras;
+    std::cout <<"\nEscribe las horas extras diurnas.\n";
+    std::cin >>HorasExtrasD;
+    std::cout <<"\nEscribe las horas extras nocturnas.\n";
+    std::cin >>HorasExtrasN;
+    std::cout <<"\nEscribe las horas extras dominicales diurnas.\n";
+    std::cin >>HorasExtrasDomD;
+    SalarioTotal = ((1.25*HorasExtrasD)+(1.75*HorasExtrasN)+(1.75*HorasExtrasDomD))*SalarioHoras;
+    std::cout <<"\nEl salario total de horas extras es= " <<SalarioTotal;
 }
